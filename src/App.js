93f1b308884f181d575031b2ecbe887894a5f780
ehsan mashali <ehsanmashali222.78@gmail.com>
2021-04-26 12:33:06 +0430

    Create Login form in my Component and Crreate server.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+// import Components
+import Dashboard from "./Components/Dashboard";
+import Preferences from "./Components/Preferences";
+import Login from "./Components/Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Preferences.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Preferences() {
+  return <h2>Preferences</h2>;
+}
+
+export default Preferences;
